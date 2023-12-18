2b0f46de53331df28aa78b70f5947c34758cc5ba
funMango <138420832+funMango@users.noreply.github.com>
2023-12-18 14:10:11 +0900

    first commit

---
+++ UIElement/UIElement/UIElementApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIElementApp.swift
+//  UIElement
+//
+//  Created by 이민호 on 12/18/23.
+//
+
+import SwiftUI
+
+@main
+struct UIElementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
